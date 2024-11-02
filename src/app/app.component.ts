38d4fe0f53145bc135bc09360bbee26ccf244c3d
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-11-02 14:00:20 -0500

    wip: hydration errors to be worked on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, BannerComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-banner',
+  imports: [],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.scss'
+})
+export class BannerComponent {
+
+}
